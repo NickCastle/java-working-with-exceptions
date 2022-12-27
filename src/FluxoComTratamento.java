7766304c34e94581ebd1b741c54c146fcc631a68
Nick <nicolas.vitor1995sv@yahoo.com>
2022-12-27 15:21:05 -0300

    working with exceptions

--- src/FluxoComTratamento.java
+++ src/FluxoComTratamento.java
@@ -1,15 +1,9 @@
 
-public class Fluxo {
+public class FluxoComTratamento {
 	
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
-		try{
-			metodo1();
-		}catch(ArithmeticException | NullPointerException ex) {
-			String msg = ex.getMessage();
-			System.out.println("Exception " + msg);
-			ex.printStackTrace();
-		}
+		metodo1();
 		System.out.println("Fim do main");
 		
 	}
@@ -23,10 +17,15 @@ public class Fluxo {
 	
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
+		for(int i=1; i<=5; i++) {
+			System.out.println(i);
+			try{
+				int a = i/0; 
+			}catch(ArithmeticException ex) {
+				System.out.println("Erro!");
+		}}
 		
-		throw new ArithmeticException();
-		
-		//System.out.println("Fim do metodo2");
+		 System.out.println("Fim do metodo2");
 	}
 		}
 
